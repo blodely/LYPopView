b5e3c70973e71a955653526904e10656284d96b8
骆昱 (Mac mini) <indie.luo@gmail.com>
2016-12-20 16:09:28 +0800

    Add : pop date view

---
+++ LYPopView/Classes/LYPopDate.h
@@ -0,0 +1,13 @@
+//
+//  LYPopDate.h
+//  LYPOPVIEW
+//
+//  CREATED BY LUO YU ON 2016-12-20.
+//  COPYRIGHT © 2016 LUO YU. ALL RIGHTS RESERVED.
+//
+
+#import <LYPopView/LYPopView.h>
+
+@interface LYPopDate : LYPopView
+
+@end
---
+++ LYPopView/Classes/LYPopDate.m
@@ -0,0 +1,21 @@
+//
+//  LYPopDate.m
+//  LYPOPVIEW
+//
+//  CREATED BY LUO YU ON 2016-12-20.
+//  COPYRIGHT © 2016 LUO YU. ALL RIGHTS RESERVED.
+//
+
+#import "LYPopDate.h"
+
+@implementation LYPopDate
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
