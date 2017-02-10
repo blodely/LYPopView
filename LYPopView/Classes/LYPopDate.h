0a22e17962cf344e535bb3db97762296180a688d
骆昱 (Mac mini) <indie.luo@gmail.com>
2017-02-10 14:57:07 +0800

    Modify : build date picker pop view

--- LYPopView/Classes/LYPopDate.h
+++ LYPopView/Classes/LYPopDate.h
@@ -8,6 +8,21 @@
 
 #import <LYPopView/LYPopView.h>
 
+typedef void (^ didSelectDate)(NSDate *date);
+
 @interface LYPopDate : LYPopView
+
+/**
+ message label
+ */
+@property (nonatomic, weak) UILabel *message;
+@property (nonatomic, weak) UIDatePicker *picker;
+
+@property (nonatomic, strong) NSString *formatter;
+@property (nonatomic, strong) NSString *timezone;
+
++ (void)showPopWithTitle:(NSString *)title datePickerMode:(UIDatePickerMode)mode stringFormat:(NSString *)formatter timezone:(NSString *)timezone andSelectionBlock:(didSelectDate)selectBlock;
+
+- (void)setSelectBlock:(didSelectDate)selectBlock;
 
 @end
