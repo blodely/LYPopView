c0551a7736a38f19020de8826bbf96e0d3dd1cf7
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-06-25 15:03:50 +0800

    [Add] demo tab picker view controller

---
+++ Example/LYPopView/_scene/tab_picker/TabPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabPickerViewController.h
+//  LYPOPVIEW
+//
+//  CREATED BY LUO YU ON 2017-06-25.
+//  COPYRIGHT © 2016 LUO YU. ALL RIGHTS RESERVED.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabPickerViewController : UIViewController
+
+@end
---
+++ Example/LYPopView/_scene/tab_picker/TabPickerViewController.m
@@ -0,0 +1,37 @@
+//
+//  TabPickerViewController.m
+//  LYPOPVIEW
+//
+//  CREATED BY LUO YU ON 2017-06-25.
+//  COPYRIGHT © 2016 LUO YU. ALL RIGHTS RESERVED.
+//
+
+#import "TabPickerViewController.h"
+
+@interface TabPickerViewController ()
+
+@end
+
+@implementation TabPickerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
