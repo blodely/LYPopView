75ff56de3a6c8101e272ab845ab5270a384c5c50
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-06-29 15:06:38 +0800

    [Modify] view initialization

--- Example/LYPopView/_scene/tab_picker/TabPickerViewController.m
+++ Example/LYPopView/_scene/tab_picker/TabPickerViewController.m
@@ -14,24 +14,21 @@
 
 @implementation TabPickerViewController
 
+- (instancetype)init {
+	if (self = [super initWithNibName:@"TabPickerViewController" bundle:[NSBundle mainBundle]]) {
+	}
+	return self;
+}
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+	[super viewDidLoad];
+	// DO ANY ADDITIONAL SETUP AFTER LOADING THE VIEW FROM ITS NIB.
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	[super didReceiveMemoryWarning];
+	// DISPOSE OF ANY RESOURCES THAT CAN BE RECREATED.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
