d203dea43e2cfcf004d5790a642ac4b25f1a3558
Luo Yu <luoyu@luoyu.space>
2018-09-19 16:08:16 +0800

    [Modify] style

--- LYPopView/Classes/action/LYPopActionView.m
+++ LYPopView/Classes/action/LYPopActionView.m
@@ -82,6 +82,7 @@ typedef void(^ buttonActionBlock)(void);
 		buttonZero.frame = (CGRect){0, vCont.bounds.size.height - 44, vCont.bounds.size.width, 44};
 		buttonZero.hidden = YES;
 		[buttonZero setBackgroundColor:[UIColor colorWithHex:conf[@"popview-theme-color"][confValue] andAlpha:1.0]];
+		[buttonZero setTitleColor:[UIColor colorWithHex:conf[@"popview-title-color"][confValue] andAlpha:1.0] forState:UIControlStateNormal];
 		[buttonZero addTarget:self action:@selector(buttonSingleIdxZeroAction:) forControlEvents:UIControlEventTouchUpInside];
 		[vCont addSubview:buttonZero];
 		btnSingleIdxZero = buttonZero;
@@ -93,6 +94,7 @@ typedef void(^ buttonActionBlock)(void);
 		buttonZero.frame = (CGRect){0, vCont.bounds.size.height - 44, vCont.bounds.size.width / 2 - (1/[UIScreen mainScreen].scale), 44};
 		buttonZero.hidden = YES;
 		[buttonZero setBackgroundColor:[UIColor colorWithHex:conf[@"popview-theme-color"][confValue] andAlpha:1.0]];
+		[buttonZero setTitleColor:[UIColor colorWithHex:conf[@"popview-title-color"][confValue] andAlpha:1.0] forState:UIControlStateNormal];
 		[buttonZero addTarget:self action:@selector(buttonDoubleIdxZeroAction:) forControlEvents:UIControlEventTouchUpInside];
 		[vCont addSubview:buttonZero];
 		btnDoubleIdxZero = buttonZero;
@@ -101,9 +103,14 @@ typedef void(^ buttonActionBlock)(void);
 		buttonOne.frame = (CGRect){vCont.bounds.size.width / 2, vCont.bounds.size.height - 44, vCont.bounds.size.width / 2, 44};
 		buttonOne.hidden = YES;
 		[buttonOne setBackgroundColor:[UIColor colorWithHex:conf[@"popview-theme-color"][confValue] andAlpha:1.0]];
+		[buttonOne setTitleColor:[UIColor colorWithHex:conf[@"popview-title-color"][confValue] andAlpha:1.0] forState:UIControlStateNormal];
 		[buttonOne addTarget:self action:@selector(buttonDoubleIdxOneAction:) forControlEvents:UIControlEventTouchUpInside];
 		[vCont addSubview:buttonOne];
 		btnDoubleIdxOne = buttonOne;
+	}
+	
+	{
+		btnSingleIdxZero.titleLabel.font = btnDoubleIdxZero.titleLabel.font = btnDoubleIdxOne.titleLabel.font = [UIFont systemFontOfSize:14];
 	}
 }
 
